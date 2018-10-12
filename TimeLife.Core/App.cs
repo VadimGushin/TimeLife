20c5430bc77a25fe86b028fc6ddf48ccc05ce722
Steve Gregory <stephencgregory@gmail.com>
2018-10-12 08:37:56 +0300

    Setup MvvmCross for Core

---
+++ TimeLife.Core/App.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeLife.Core.ViewModels;
+
+namespace TimeLife.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            //Mvx.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<MainViewModel>();
+        }
+    }
+}
---
+++ TimeLife.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeLife.Core.ViewModels
+{
+    public class MainViewModel: MvxViewModel
+    {
+
+        public override Task Initialize()
+        {
+            TestValue = "Test String From View Model";
+            return base.Initialize();
+
+        }
+        private string _testValue;
+        public string TestValue
+        {
+            get => _testValue;
+            set
+            {
+                _testValue = value;
+                RaisePropertyChanged(() => TestValue);
+            }
+        }
+
+    }
+}
